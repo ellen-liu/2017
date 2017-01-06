351a740e5db3ef70d944f0e967ecf9a9847beb0b
Ellen <eliu827@gmail.com>
2017-01-06 10:21:58 -0500

    program for palindromes

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+# Program to check if the given string is a palindrome or not
+
+word = raw_input("Enter a string: ")
+def palindrome(word):
+	if word == word[::-1]:
+		print "%s is a palindrome :)" % word
+	else:
+		print "%s is not a palindrome :(" % word
+
+palindrome(word)
